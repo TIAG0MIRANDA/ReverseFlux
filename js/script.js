0b291fe4735b1820e6467dfd3f44f7f530e4988e
Tiago Miranda <t.miranda0249@gmail.com>
2024-10-19 16:32:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,14 +2,12 @@ document.getElementById("invertButton").addEventListener("click", function () {
     var inputText = document.getElementById("inputText").value;
     var outputText = document.getElementById("outputText");
 
-    // Função para inverter cada palavra
     function inverterPalavras(text) {
         return text.split(' ').map(function (palavra) {
             return palavra.split('').reverse().join('');
         }).join(' ');
     }
 
-    // Inverte as palavras e exibe o resultado
     var textoInvertido = inverterPalavras(inputText);
     outputText.innerHTML = textoInvertido;
 });
